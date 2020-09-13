181dbf0e168b457506788ff8adbbf01bfb909965
Red Fairy <agorelik2@gmail.com>
2020-09-13 14:42:23 -0400

    create orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,94 @@
 //import mysql connection.
 const connection = require("../config/connection.js");
 
+//orm object
+const orm = {
+  //select all
+  selectAll: function (tableInput, cb) {
+    let queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  //insert one
+  insertOne: function (table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  //update one
+  updateOne: function (table, objColVals, condition, cb) {
+    let queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  deleteOne: function (table, condition, cb) {
+    let queryString = "DELETE FROM " + TABLE + " WHERE " + condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+//orm helper functions
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+// helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    //check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      //if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      //e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      //e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  //translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
 //export orm object
 module.exports = orm;
