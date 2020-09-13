ae10b1dd3afc3ef2e0bf32482a52446e44aca9e5
Red Fairy <agorelik2@gmail.com>
2020-09-13 18:20:15 -0400

    updated models/burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,28 @@
 //import orm
 const orm = require("../config/orm.js");
 //burger model functions
-const burger = {};
+const burger = {
+  all: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  delete: function (condition, cb) {
+    orm.deleteOne("burgers", condition, function (res) {
+      cb(res);
+    });
+  },
+};
 
 //export the burger model functions
 module.exports = burger;
